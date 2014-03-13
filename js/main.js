687b4f1c0c45a94f5a7c58e30b31dcdb4721eadc
baldur <baldur.gudbjornsson@gmail.com>
2014-03-13 11:24:32 -0400

    getting rid of tabbed spaces and minor tweaks

--- js/main.js
+++ js/main.js
@@ -1,90 +1,88 @@
 var SAAgent, SASocket;
 var CHANNELID = 104;
-var ProviderAppName = "GearProvider";
+var ProviderAppName = "MapzenGearProvider";
 
 function mapzenDebug(msg) {
-	console.log("MapzenGear:" + msg)
+    console.log("MapzenGear:" + msg)
 }
 
 function onerror(err) {
-	mapzenDebug(err);
+    mapzenDebug(err);
 }
 
 function onreceive(channelId, data) {
-	var json = $.parseJSON(data);
-	if (json.connected) {
-		mapzenDebug("Connected to device");
-	} else {
-		mapzenDebug("applying json: " + data);
+    var json = $.parseJSON(data);
+    if (json.connected) {
+        mapzenDebug("Connected to device");
+    } else {
+        mapzenDebug("applying json: " + data);
         new Ractive({
            el: 'navigation',
            template: '#routeTemplate',
            data: json
         });
-	}
+    }
 }
 
 var agentCallback = {
-	onconnect : function(socket) {
-		SASocket = socket;
-		SASocket.setDataReceiveListener(onreceive);
-		SASocket.sendData(CHANNELID, "{\"connected\" : true}");
-	},
-	onerror : onerror
+    onconnect : function(socket) {
+        SASocket = socket;
+        SASocket.setDataReceiveListener(onreceive);
+        SASocket.sendData(CHANNELID, "{\"connected\" : true}");
+    },
+    onerror : onerror
 };
 
 var peerAgentFindCallback = {
-	onpeeragentfound : function(peerAgent) {
-		try {
-			if (peerAgent.appName == ProviderAppName) {
-				SAAgent.setServiceConnectionListener(agentCallback);
-				SAAgent.requestServiceConnection(peerAgent);
-			}
-			else {
-				mapzenDebug("Not expected app!! : " + peerAgent.appName);
-			}
-		}
-		catch(err) {
-			mapzenDebug(err);
-		}
-	},
-	onerror : onerror
+    onpeeragentfound : function(peerAgent) {
+        try {
+            if (peerAgent.appName == ProviderAppName) {
+                SAAgent.setServiceConnectionListener(agentCallback);
+                SAAgent.requestServiceConnection(peerAgent);
+            } else {
+                mapzenDebug("Not expected app!! : " + peerAgent.appName);
+            }
+        } catch(err) {
+            mapzenDebug(err);
+        }
+    },
+    onerror : onerror
 };
 
 function onsuccess(agents) {
-	mapzenDebug("agents count: " + agents.length);
-	try {
-		if (agents.length > 0) {
-			SAAgent = agents[0];
-			SAAgent.setPeerAgentFindListener(peerAgentFindCallback);
-			SAAgent.findPeerAgents();
-		} else {
-			mapzenDebug("Not found SAAgent!!");
-		}
-	} catch(err) {
-		mapzenDebug(err);
-	}
+    mapzenDebug("agents count: " + agents.length);
+    try {
+        if (agents.length > 0) {
+            SAAgent = agents[0];
+            SAAgent.setPeerAgentFindListener(peerAgentFindCallback);
+            SAAgent.findPeerAgents();
+        } else {
+            mapzenDebug("Not found SAAgent!!");
+        }
+    } catch(err) {
+        mapzenDebug(err);
+    }
 }
 
 function connect() {
-	if (SASocket) {
-		mapzenDebug('Already connected!');
+    if (SASocket) {
+        mapzenDebug('Already connected!');
         return false;
     }
-	try {
-		mapzenDebug("conneting");
-		webapis.sa.requestSAAgent(onsuccess, onerror);
-	} catch(err) {
-		mapzenDebug(err);
-	}
+    try {
+        mapzenDebug("conneting");
+        webapis.sa.requestSAAgent(onsuccess, onerror);
+    } catch(err) {
+        mapzenDebug(err);
+    }
 }
 
-window.onload = function () {
-	mapzenDebug("onload");
-	connect();
+$(function () {
+    mapzenDebug("onload");
+    connect();
     document.addEventListener('tizenhwkey', function(e) {
         if(e.keyName == "back") {
             tizen.application.getCurrentApplication().exit();
         }
     });    
-};
+});
